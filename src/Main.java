fbd9feb9ec1e0c36747435dfdc46c7926867b781
s693087 <carsonpoteet4@gmail.com>
2022-12-05 13:12:06 -0600

    adsfd

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int place = 0;
+        while(place !=100)
+            move(place);
 
     }
+
+    public static int move(int sum) {
+        int dice = (int)(Math.random()*7);
+        //if(sum+dice<=100)
+            return sum + dice;
+    }
+
 }
