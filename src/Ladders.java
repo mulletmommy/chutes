689264ae557ec23b24a2d838bedb954b2dfc8fbc
djasjdasjfsa <djasjdasjfsa>
2022-12-05 13:10:43 -0600

    background

---
+++ src/Ladders.java
@@ -0,0 +1,69 @@
+public class Ladders {
+
+    private int [][] mat;
+    private boolean frue;
+
+    public Ladders(){
+        mat = new int[10][10];
+
+    }
+
+    public boolean hitit(int z){
+        if(z == 4){
+            frue = true;
+        }
+        if(z == 13){
+            frue = true;
+        }
+        if(z == 50){
+            frue = true;
+        }
+        if(z == 33){
+            frue = true;
+        }
+        if(z == 42){
+            frue = true;
+        }
+        if(z == 62){
+            frue = true;
+        }
+        if(z == 74){
+            frue = true;
+        }
+        else{
+            frue = false;
+        }
+        return frue;
+    }
+
+    public int (boolean x){
+        if(x = true){
+            if(z == 4){
+                frue = true;
+            }
+            if(z == 13){
+                frue = true;
+            }
+            if(z == 50){
+                frue = true;
+            }
+            if(z == 33){
+                frue = true;
+            }
+            if(z == 42){
+                frue = true;
+            }
+            if(z == 62){
+                frue = true;
+            }
+            if(z == 74){
+                frue = true;
+            }
+            else{
+                frue = false;
+            }
+        }
+
+    }
+
+}
