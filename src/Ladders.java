4ee38adef35e4585cf207d3314adc15565161271
s693087 <carsonpoteet4@gmail.com>
2022-12-07 13:35:55 -0600

    adsfd

--- src/Ladders.java
+++ src/Ladders.java
@@ -8,58 +8,31 @@ public class Ladders {
 
     }
 
-    public boolean hitit(int z){
+    public static void hitit(int z){
         if(z == 4){
-            frue = true;
+            z = 25;
         }
         if(z == 13){
-            frue = true;
+            z = 46;
         }
         if(z == 50){
-            frue = true;
+            z = 69;
         }
         if(z == 33){
-            frue = true;
+            z = 49;
         }
         if(z == 42){
-            frue = true;
+            z = 63;
         }
         if(z == 62){
-            frue = true;
+            z = 81;
         }
         if(z == 74){
-            frue = true;
+            z = 92;
         }
-        else{
-            frue = false;
-        }
-        return frue;
+
     }
 
-    public int jeasoi(boolean x, int z){
-        if(x = true){
-            if(z == 4){
-                z = 25;
-            }
-            if(z == 13){
-                z = 46;
-            }
-            if(z == 50){
-                z = 69;
-            }
-            if(z == 33){
-                z = 49;
-            }
-            if(z == 42){
-                z = 63;
-            }
-            if(z == 62){
-                z = 81;
-            }
-            if(z == 74){
-                z = 92;
-            }
-        }
-    }
+
 
 }
